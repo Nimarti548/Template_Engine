345d1fa8d925071b461fd5ef02a05243425d36d3
Nicolas Martinez <Nimarti548@gmail.com>
2020-12-10 15:51:01 -0800

    UPDATE: Fix indentation in app.js

--- Develop/app.js
+++ Develop/app.js
@@ -11,41 +11,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
 let employees = [];
 
 function getEmployeeClass(){
     inquirer.prompt([
     {
-      type: "list",
-      message: "Choose employee's job classification.",
-      name: "classInput",
-      choices: ["Manager", "Engineer", "Intern"],
+        type: "list",
+        message: "Choose employee's job classification.",
+        name: "classInput",
+        choices: ["Manager", "Engineer", "Intern"],
     },
-  ])
+    ])
     .then((res) => {
     if(res.classInput === "Manager") {
         addManager()
@@ -97,12 +73,12 @@ function addManager () {
         message: "Enter Manager office number.",
         name: "managerOfficeNum",
     },
-  ])
-  .then((managerRes) => {
+    ])
+    .then((managerRes) => {
     const newManager = new Manager(managerRes.managerName, managerRes.managerID, managerRes.managerEmail, managerRes.managerOfficeNum);
     employeeArr.push(newManager);
     addNewMember()
- })
+    })
 }
 
 function renderMain () {
